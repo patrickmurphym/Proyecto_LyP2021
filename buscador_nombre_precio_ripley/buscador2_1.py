8ba181b6f806217deadfeb2ad888e21a42564787
Xavi-Merino <thenightblack@gmail.com>
2021-06-27 02:59:44 -0400

    /buscador_nombre_precio_ripley/buscador2_1.py imprime todos los nombres y precios de los telefonos en la primera pagina, falta implementar que pase de pagina y guardar los datos en un objeto llamado telefono con propiedades como precio_oferta, precio_normal, Nombre , etc ...

--- buscador_nombre_precio_ripley/buscador2_1.py
+++ buscador_nombre_precio_ripley/buscador2_1.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options 
 
-'''estas son opciones de como se abrira chroma'''
+'''estas son opciones de como se abrira chrome'''
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")  #esta es la opcion que hace que no haya UI
 options.add_argument('--ignore-certificate-errors')
@@ -14,7 +14,12 @@ driver = webdriver.Chrome(executable_path=("/usr/bin/chromedriver"), chrome_opti
 #method para ir a una pagina web
 driver.get("https://simple.ripley.cl/tecno/celulares?source=menu")
 
-#elem es un objeto que contiene las propiedades de la clase llamada 'product-price'
-elem = driver.find_element_by_class_name('catalog-container')
-precio = elem.text
-print(precio)
+#elem es un objeto(WebElement) que contiene las propiedades de la clase llamada 'product-price'(como .text)
+for i in range(0,46):
+    elem = driver.find_elements_by_class_name('catalog-product-details__name')
+    nombre = elem[i].text
+    print(nombre)
+    elem = driver.find_elements_by_class_name('catalog-prices__offer-price')
+    precio = elem[i].text
+    print(precio)
+driver.close()
