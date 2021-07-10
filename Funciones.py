01b4912670bf5305a4d2ffaabbca240330c82d56
patrickmurphym <patrickmm80@gmail.com>
2021-07-10 19:12:37 -0400

    Funcion minimo

--- Funciones.py
+++ Funciones.py
@@ -1,2 +1,8 @@
-def funcion():
+import pandas as pd
+
+def minPrice(df, tienda): 
+    printMinPrice(df.iloc[df[df['page'] == tienda]['internetPrice'].idxmin(),[0,2]], tienda)
+
+def printMinPrice(df, tienda):
+    print("En "+tienda, "el producto más barato es", df['name'], 'a un precio de', df['internetPrice'])
     return
