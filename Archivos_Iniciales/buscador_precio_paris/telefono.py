88fe65d19670efaaf1b27937892d50c604394311
patrickmurphym <patrickmm80@gmail.com>
2021-07-10 20:44:33 -0400

    Eliminamos archivos que no son del proyecto 1.0

--- Archivos_Iniciales/buscador_precio_paris/telefono.py
+++
@@ -1,7 +0,0 @@
-class Telefono(object):
-
-    def __init__(self, nombre, precio_tienda, precio_internet, precio_tarjeta):
-        self.nombre = nombre
-        self.precio_tienda = precio_tienda
-        self.precio_internet = precio_internet
-        self.precio_tarjeta = precio_tarjeta
